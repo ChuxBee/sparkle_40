ed6f4deea89a918ea5f213838177952aed00caa5
chuks <chuxblueson@gmail.com>
2024-03-04 20:05:46 -0500

    modified css

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
+let navigation = document.querySelector('.navigation');
+let toggle = document.querySelector('.toggle');
+
+toggle.onclick = function () {
+  navigation.classList.toggle('active')
+  toggle.classList.toggle('active')
+}
+
 window.addEventListener('scroll', function () {
-    const header = document.querySelector('header');
-    header.classList.toggle("sticky", window.scrollY > 0)
-  })
+  const header = document.querySelector('header');
+  header.classList.toggle("sticky", window.scrollY > 0)
+})
